1f6c10c09044f9c303e75905f5ca5f7de3f1ff6e
Shubang Sridhar <shubangsridhar@gmail.com>
2016-03-13 17:58:58 -0400

    Added new invKin method for 3 axis robot. Refining logic to take into account cases where Z-axis of following joint may not be aligned. Have not tested yet.

--- RobotDef.m
+++ RobotDef.m
@@ -25,5 +25,34 @@ classdef RobotDef < handle
               T = T*obj.links(i).linkTransform();
           end
        end
+       
+       function JointAngles = invKinematics_3(obj, goalState)
+          assert(obj.num_links==4, 'Need number of robot links to be 4. Do not forget the base to joint1 link');
+          for i=1:obj.num_links
+              assert(obj.links(i).type==0, 'All links must be revolute type');
+          end
+          JointAngles = zeros(1, 3);
+          x = goalState(1, 4);
+          y = goalState(2, 4);
+          z = goalState(3, 4);
+          r = sqrt(x^2+y^2);
+          l = zeros(1, 3);
+          for i=2:4
+             if(obj.links(i).alpha==0 || obj.links(i).alpha==pi || obj.links(i).alpha==-pi)
+                 l(i) = obj.links(i).d;
+             else
+                 l(i) = obj.links(i).a;
+             end
+          end
+          
+          JointAngles(1) = atan2(y, x);
+          
+          c3 = (x^2+y^2+(z-l(1))^2-l(2)^2-l(3)^2)/(2*l(2)*l(3));
+          s3 = sqrt(1-c3^2);
+          JointAngles(3) = atan2(s3, c3);
+          
+          JointAngles(2) = atan2(z-l(1), r)-atan2(l(3)*s3, l2+l(3)*c(3));
+          
+       end
    end
 end
