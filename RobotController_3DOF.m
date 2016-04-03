702f58386f4443b75127d59d5979b4ec82eab31f
Shubang Sridhar <shubangsridhar@gmail.com>
2016-04-03 18:43:37 -0400

    Updated script to have robot definition in robot controller

--- RobotController_3DOF.m
+++ RobotController_3DOF.m
@@ -6,6 +6,7 @@ classdef RobotController_3DOF < handle
         arduino_handle
         joint_servos
         other_servos
+        robot
     end
     methods (Access = private)
         function [servos] = servos_initialize(obj, minPulseDuration, maxPulseDuration, num_servos, pin_list)
@@ -17,30 +18,35 @@ classdef RobotController_3DOF < handle
         end
     end
     methods (Access = public)
-        function robotController = RobotController_3DOF(pulse_duration, arduino_pin_list)
+        function robotController = RobotController_3DOF(pulse_duration, arduino_pin_list, links)
             assert(length(arduino_pin_list)==3, 'Program currently only supports 3 servo robots');
             assert(length(pulse_duration)==2, 'Pulse duration includes one min and one max. Length should be 2');
             robotController.arduino_handle = arduino();
-            robotController.joint_servos = robotController.servos_initialize(pulse_duration(1), pulse_duration(2), length(arduino_pin_list), arduino_pin_list);
+            robotController.joint_servos = robotController.servos_initialize(pulse_duration(1), pulse_duration(2), ...
+                length(arduino_pin_list), arduino_pin_list);
+            robotController.robot = RobotDef(links);
         end
         function [] = go_home(obj)
             for i=1:length(obj.joint_servos)
                 writePosition(obj.joint_servos(i), 0.0);
+                pause(0.5);
             end
         end
         function [] = sweep_demo(obj)
-            theta = 0;
+            theta = 1;
             delta = 1;
-            while theta>-1
+            loop = 0;
+            while loop<1000
                 for i=1:length(obj.joint_servos)
                     writePosition(obj.joint_servos(i), theta/180);
                 end
                 theta = theta + delta;
-                if theta==180
+                if theta==179
                     delta = -1;
-                elseif theta == 0
+                elseif theta == 1
                     delta = 1;
                 end
+                loop = loop+1;
             end
         end
     end
--- test_script.m
+++ test_script.m
@@ -1,12 +1,19 @@
 clear
 clc
 
+%% Initial parameters for robot
 pulse_duration = [600*10^-6  2450*10^-6];
 pin_numbers = [9, 10, 11];
+L(1) = LinkDef([0,     69,     0,     pi/2,    0]);
+L(2) = LinkDef([0,     22,     85,   -pi,      0]);
+L(3) = LinkDef([0,     22,     60,    0,       0]);
 
-robot_controller = RobotController_3DOF(pulse_duration, pin_numbers);
+%% INitializing arduino robot
+robot_controller = RobotController_3DOF(pulse_duration, pin_numbers, L);
 pause(2.0);
+
+%% Asking robot to go home first
 robot_controller.go_home();
-pause(2.0);
-robot_controller.sweep_demo();
+
+%% Clearing all variables
 clear
